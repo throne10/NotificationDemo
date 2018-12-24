f1c16b96b2511bc2785cb4d33f2b65bbcfb2b0a4
yuxd <349434222@qq.com>
2018-12-24 09:48:23 +0800

    1231231231

--- app/src/main/java/ezy/demo/notify/MainActivity.java
+++ app/src/main/java/ezy/demo/notify/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ActivityMainBinding binding;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/ezy/demo/notify/a.java
@@ -0,0 +1,8 @@
+package ezy.demo.notify;
+
+/**
+ * Created by Administrator on 2018/12/24.
+ */
+
+public class a {
+}
